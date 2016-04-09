55232df326b846e413bfc2e15403ebcf692d50f0
Gustavo Gonzalez <ggonzalezsomer@gmail.com>
2016-04-09 18:16:21 -0300

    Añadiendo testcase

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calculadora-HelloWorld
+##Grupo: HelloWorld
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,7 @@
+/*
+ * Calculadora por linea de comandos
+ * Grupo HelloWorld
+ */
 package main;
 
 import java.util.Scanner;
@@ -5,12 +9,27 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Ingrese la operacion a realizar: ");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese la suma: ");
 		String input = sc.nextLine();
 		Calculadora calc = new Calculadora(input);
-		float resultado;
-		resultado = calc.Suma();
+		float resultado = 0; //resultado de la operacion
+		//Checkeo que operacion voy a realizar
+		if (input.contains("+")){
+			resultado = calc.Suma();
+		}
+		else if(input.contains("-")){
+			//resultado = calc.Resta();
+		}
+		else if(input.contains("*")){
+			//resultado = calc.Multiplica();
+		}
+		else if(input.contains("/")){
+			//resultado = calc.Divide()
+		}
+		else{
+			//resultado = calc.Modulo();
+		}
 		sc.close();
 		System.out.printf("%n%f", resultado);
 	}
