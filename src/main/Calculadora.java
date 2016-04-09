8226123dfafcf90df084b5daca7f9ac7fc3fac4a
Facundo Maero <facundojmaero@gmail.com>
2016-04-09 20:53:59 -0300

    correcciones cosmeticas

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -9,27 +9,22 @@ public class Calculadora {
 
 	public Calculadora(String input) {
 		sc = new Scanner(input);
-		
-		this.input = input.replaceAll("\\s","");
-		
-//		num = this.input.length();	
+		this.input = input.replaceAll("\\s", "");
 	}
 
-
-	
 	public float sumaResta() {
 		float result = sc.nextFloat();
-		while(sc.hasNext()){	//for(int i=0;i<num-2;i=i+2){
+		while (sc.hasNext()) {
 			String op = sc.next();
 			float B = sc.nextFloat();
-			switch(op){
+			switch (op) {
 			case "+":
-				result = result+B;
+				result = result + B;
 				break;
 			case "-":
-				result = result-B;
+				result = result - B;
 				break;
-				
+
 			}
 		}
 		sc.close();
--- src/main/Main.java
+++ src/main/Main.java
@@ -15,33 +15,30 @@ public class Main {
 		String input = sc.nextLine();
 		input = procesar(input);
 		Calculadora calc = new Calculadora(input);
-		float resultado = 0; //resultado de la operacion
-		
-		//Checkeo que operacion voy a realizar
-		if (input.contains(" + ")||input.contains(" - ")){
-			try{
+		float resultado = 0; // resultado de la operacion
+
+		// Checkeo que operacion voy a realizar
+		if (input.contains(" + ") || input.contains(" - ")) {
+			try {
 				resultado = calc.sumaResta();
-			}
-			catch(InputMismatchException e){
+			} catch (InputMismatchException e) {
 				System.out.printf("Formato incorrecto");
 			}
-		}
-		else if(input.contains("*")){
-			//resultado = calc.multiplica();
-		}
-		else if(input.contains("/")){
-			//resultado = calc.divide()
-		}
-		else{
-			//resultado = calc.modulo();
+		} else if (input.contains("*")) {
+			// resultado = calc.multiplica();
+		} else if (input.contains("/")) {
+			// resultado = calc.divide()
+		} else {
+			// resultado = calc.modulo();
 		}
 		sc.close();
 		System.out.printf("El resultado es: %.2f", resultado);
 	}
-	
-	private static String procesar(String input){
-		input = input.replace("+"," + ");
-		input = input.replace("-"," - ");
+
+	private static String procesar(String input) { 	// agrega separadores entre
+													// operadores
+		input = input.replace("+", " + ");
+		input = input.replace("-", " - ");
 		return input;
 	}
 }
